a84cac727d032d3475b0d5185ebf1a372a6404ca
sandaniW <sandaniwickramanayake04@gmail.com>
2025-05-06 15:54:19 +0530

    simple c# program

---
+++ TestProject/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+Console.WriteLine("Hello, c#!");
+//using net libraries
+Random dice = new Random();
+//Random dice = new(); -> simplified way of writign
+int roll = dice.Next(1,7);
+Console.WriteLine(roll);
