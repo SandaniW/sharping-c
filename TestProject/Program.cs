c884e5f087f2c4a4ec1f8603f635481ba90e6f17
sandaniW <sandaniwickramanayake04@gmail.com>
2025-05-13 12:04:59 +0530

    testproject update and business rules(challenge activity)

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -5,4 +5,50 @@ Console.WriteLine("Hello, c#!");
 Random dice = new Random();
 //Random dice = new(); -> simplified way of writign
 int roll = dice.Next(1,7);
-Console.WriteLine(roll);
+
+Console.WriteLine($"Dice No: {roll}");
+//Math class methods
+int firstN = 500;
+int secondN = 600;
+int largerValue = System.Math.Max(firstN,secondN);
+Console.WriteLine(largerValue);
+
+//decision logic - if
+int roll1 = dice.Next(1,7);
+int roll2 = dice.Next(1,7);
+int roll3 = dice.Next(1,7);
+
+int total  = roll+roll2+roll3;
+Console .WriteLine($"{roll} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+    Console.WriteLine($"Your total including the bonus: {total}");
+}
+
+if (total >= 16)
+{
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
+}
